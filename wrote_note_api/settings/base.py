95c22662b2299b351696721a82ea218112a2619d
William Dana <william.b.dana@gmail.com>
2019-01-02 23:34:03 -0500

    Update allowed hosts

--- wrote_note_api/settings/base.py
+++ wrote_note_api/settings/base.py
@@ -38,7 +38,7 @@ SECRET_KEY = get_secret_setting('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.8']
 
 
 # Application definition
