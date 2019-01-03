65f999df189f19a218582e53e96b74ce19dcd885
William Dana <william.b.dana@gmail.com>
2019-01-02 19:44:00 -0500

    Adjust manage.py to reflect current env and create migrations

--- wrote_note_api/apps/notes/models.py
+++ wrote_note_api/apps/notes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    content = models.TextField()
